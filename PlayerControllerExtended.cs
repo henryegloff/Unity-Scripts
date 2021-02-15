8528509ec50678bdf5b18f5aa83d4171d0deeb1c
Henry Egloff <henryegloff@users.noreply.github.com>
2021-02-15 12:29:57 +1100

    Update and rename SimplePlayerController.cs to PlayerControllerExtended.cs

--- PlayerControllerExtended.cs
+++ PlayerControllerExtended.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimplePlayerController : MonoBehaviour
+public class PlayerControllerExtended : MonoBehaviour
 {
 
     private Rigidbody rb;
