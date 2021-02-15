98292bfb0e85b0453849fcbeea5b364efb0972e2
Henry Egloff <henryegloff@users.noreply.github.com>
2021-02-15 12:25:33 +1100

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -5,28 +5,30 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject target;
+    public GameObject player; 
+    
+    /* 
+    You will need to set the player object in Unity. 
+    The object that has this script attached to it, ie the 'Camera Target' object
+    will then align itself with whatever object is set as the 'Player'
+    */
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.position = target.transform.position;
-
+        
+        // Align to player position
+        transform.position = player.transform.position;
+        
+        // Rotate left
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * 100, Space.World);
         }
-
+        
+        // Rotate right
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 100, Space.World);
         }
-
     }
 }
