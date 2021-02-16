59899de6f149cc27ae6c1851a68bcb5bde4461bf
Henry Egloff <henryegloff@users.noreply.github.com>
2021-02-16 18:06:01 +1100

    Update and rename TrackObject.cs to TrackObjectPosition.cs

--- TrackObjectPosition.cs
+++ TrackObjectPosition.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackObject : MonoBehaviour
+public class TrackObjectPosition : MonoBehaviour
 {
 
     private Rigidbody rb;
