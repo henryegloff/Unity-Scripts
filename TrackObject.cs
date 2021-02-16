94397f168e32402b8d270f3a1e6946204368d4cd
Henry Egloff <henryegloff@users.noreply.github.com>
2021-02-16 17:52:31 +1100

    Update and rename TrackPlayer.cs to TrackObject.cs

--- TrackObject.cs
+++ TrackObject.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackPlayer : MonoBehaviour
+public class TrackObject : MonoBehaviour
 {
 
     private Rigidbody rb;
-    public Transform target;
+    public Transform target; // The object that will be tracked
 	
     void Start()
     {
@@ -16,6 +16,6 @@ public class TrackPlayer : MonoBehaviour
     void Update()
     {
         transform.LookAt(target);
-        rb.AddForce(transform.forward * 1);
+        //rb.AddForce(transform.forward * 1); // Apply forwards force to move the object towards the target
     }
 }
