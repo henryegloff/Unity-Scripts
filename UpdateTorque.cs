a08aa1416eaa572a3b82dfc51bffa03b97be15f6
Henry Egloff <henryegloff@users.noreply.github.com>
2021-02-22 21:33:26 +1100

    Update UpdateTorque.cs

--- UpdateTorque.cs
+++ UpdateTorque.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class UpdateTorque : MonoBehaviour
 {
 
-	/* 
+    /* 
 
-	This script adds torque to an rigid body object that 
-	keeps updating until it reaches terminal velocity.
+    This script adds torque to an rigid body object that 
+    keeps updating until it reaches terminal velocity.
 
-	It is useful for physical objects in the game
-	that will interact with other objects.
+    It is useful for physical objects in the game
+    that will interact with other objects.
 
-	It seems to work best when the Position and Rotation
-	is frozen on all axis' except the torque axis,
-	and when the object is not right on a ground plane.
+    It seems to work best when the Position and Rotation
+    is frozen on all axis' except the torque axis,
+    and when the object is not right on a ground plane.
 
-	Also for the object to have a high mass, eg Cube:
-	Scale = 10, 2, 2
-	Torque Y = 22
+    Also for the object to have a high mass, eg Cube:
+    Scale = 10, 2, 2
+    Torque Y = 22
 	
     */
 
