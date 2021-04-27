7cc2ec35c4db2b55d11093aba97f79cddac70582
Henry Egloff <henryegloff@users.noreply.github.com>
2021-04-27 15:23:27 +1000

    Create HoverFlight.cs

---
+++ HoverFlight.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverFlight : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float forwardThrust = 88;
+    public float verticalThrust = 88;
+    public float horizontalThrust = 88;
+    public float steeringIntensity = 2;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    	// Establish directions
+        var upDirection = rb.transform.up;
+        var forwardDirection = rb.transform.forward;
+        var RightDirection = rb.transform.right;
+
+        // Forwards
+        if (Input.GetKey(KeyCode.W) )
+        {
+        	rb.AddForce(forwardDirection * forwardThrust);
+        }
+
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            rb.AddForce(RightDirection * forwardThrust);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.AddForce(-RightDirection * forwardThrust);
+        }
+
+        // Upwards
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(upDirection * verticalThrust);
+        }
+
+        // Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * steeringIntensity * 100, Space.World);
+            //transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * steeringIntensity * 50, Space.World);
+        }
+        
+        
+        // Right
+        if (Input.GetKey(KeyCode.D))
+        {
+        	transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * steeringIntensity * 100, Space.World);
+        }
+
+        
+        // Downwards force 
+        if (Input.GetKey(KeyCode.X))
+        {
+            rb.AddForce(-upDirection * verticalThrust);
+        }
+      
+
+    }
+}
