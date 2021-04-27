b54878a0514b9613a6c92ffcafa35289e0ce8d0c
Henry Egloff <henryegloff@users.noreply.github.com>
2021-04-27 15:33:12 +1000

    Update HoverFlight.cs

--- HoverFlight.cs
+++ HoverFlight.cs
@@ -19,7 +19,6 @@ public class HoverFlight : MonoBehaviour
     void Update()
     {
 
-    	// Establish directions
         var upDirection = rb.transform.up;
         var forwardDirection = rb.transform.forward;
         var RightDirection = rb.transform.right;
@@ -30,38 +29,42 @@ public class HoverFlight : MonoBehaviour
         	rb.AddForce(forwardDirection * forwardThrust);
         }
 
+        // Backwards
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-forwardDirection * forwardThrust);
+        }
 
+        // Strafe Right
         if (Input.GetKey(KeyCode.E))
         {
             rb.AddForce(RightDirection * forwardThrust);
         }
 
+        // Strafe Left
         if (Input.GetKey(KeyCode.Q))
         {
             rb.AddForce(-RightDirection * forwardThrust);
         }
 
         // Upwards
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.Space))
         {
             rb.AddForce(upDirection * verticalThrust);
         }
 
-        // Left
+        // Turn Left
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * steeringIntensity * 100, Space.World);
-            //transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * steeringIntensity * 50, Space.World);
         }
         
-        
-        // Right
+        // Turn Right
         if (Input.GetKey(KeyCode.D))
         {
         	transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * steeringIntensity * 100, Space.World);
         }
 
-        
         // Downwards force 
         if (Input.GetKey(KeyCode.X))
         {
